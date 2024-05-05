5b21d265a8f9802f8965bcb25ff753d42591417a
booblik2022 <booblik2022@gmail.com>
2024-05-05 23:35:53 +0300

    Переписал с использоанием презентера

--- src/main.js
+++ src/main.js
@@ -1,21 +1,14 @@
 import Filters from './view/filters.js';
 import Sorting from './view/sorting.js';
-import EventList from './view/add-events-list.js';
-import WayPoint from './view/way-point.js';
-import EditForm from './view/edit-form.js';
+import MainPresenter from './presenter/main-presenter.js';
 import {render} from './render.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteSortingElement = document.querySelector('.trip-events');
+const presenter = new MainPresenter({container: siteSortingElement});
 
 
 render(new Filters(), siteFilterElement);
 render(new Sorting(), siteSortingElement);
-render(new EventList(), siteSortingElement);
 
-const siteEventList = document.querySelector('.trip-events__list');
-
-render(new EditForm(), siteEventList);
-render(new WayPoint(), siteEventList);
-render(new WayPoint(), siteEventList);
-render(new WayPoint(), siteEventList);
+presenter.init();
