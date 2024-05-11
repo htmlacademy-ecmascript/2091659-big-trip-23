c5e6c2aaa998aa792641f9cff627266cc37d758b
booblik2022 <booblik2022@gmail.com>
2024-05-11 19:55:52 +0300

    Рефакторинг презентера  Отрисовку фильтра убрал в презентер

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Filters from './view/filters.js';
-import Sorting from './view/sorting.js';
 import MainPresenter from './presenter/main-presenter.js';
+import PointsModel from './model/points-model.js';
 import {render} from './render.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteSortingElement = document.querySelector('.trip-events');
-const presenter = new MainPresenter({container: siteSortingElement});
 
+const pointsModel = new PointsModel();
+pointsModel.init();
+
+const presenter = new MainPresenter({container: siteSortingElement, pointsModel: pointsModel});
 
 render(new Filters(), siteFilterElement);
-render(new Sorting(), siteSortingElement);
-
 presenter.init();
