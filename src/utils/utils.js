984a452c3f63c70cc8f6ab6e173bfdaa2cc395af
booblik2022 <booblik2022@gmail.com>
2024-05-15 00:27:25 +0300

    Добаил функции для отображения дат

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+import { DateFormat } from '../const.js';
 /**
  * Функция получения случайного элемента из массива
  * @param {*} массив
@@ -7,4 +9,15 @@ function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-export {getRandomArrayElement};
+const humanizeDate = (date, dateFormat) => date ? dayjs(date).format(dateFormat) : '';
+
+const formatFullDate = (inputDate) => //задаем дату в полном формате
+  inputDate ? dayjs(inputDate).format(DateFormat.FULL) : '';
+
+const formatShortDate = (inputDate) => //задаем месяцы
+  inputDate ? dayjs(inputDate).format(DateFormat.DATE) : '';
+
+const formatTime = (inputDate) => // задаем часы/минуты
+  inputDate ? dayjs(inputDate).format(DateFormat.TIME) : '';
+
+export {getRandomArrayElement, humanizeDate, formatFullDate, formatShortDate, formatTime};
