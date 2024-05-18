43f4aa5079d2613319a2aac682c0844d6311faef
booblik2022 <booblik2022@gmail.com>
2024-05-18 13:06:37 +0300

    Добаил приватные свойства в презентере

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -6,24 +6,37 @@ import {render} from '../framework/render.js';
 
 
 export default class MainPresenter {
-  Sorting = new Sorting();
-  EventsList = new AddEventsList();
+  #container;
+  #pointsModel;
+  #sorting = new Sorting();
+  #eventsList = new AddEventsList();
+  #editForm = new EditForm();
+  #points = [];
+  #destinationsData = [];
+  #offersData = [];
 
   constructor({container, pointsModel}) {
-    this.container = container;
-    this.pointsModel = pointsModel;
+    this.#container = container;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
-    this.points = [...this.pointsModel.getPoints()];
-    this.destinationsData = [...this.pointsModel.getDestinations()];
-    this.offersData = [...this.pointsModel.getOffers()];
-    render (this.Sorting, this.container);
-    render(this.EventsList, this.container);
-    render(new EditForm(), this.EventsList.element);
+    this.#points = [...this.#pointsModel.points];
+    this.#destinationsData = [...this.#pointsModel.destinations];
+    this.#offersData = [...this.#pointsModel.offers];
+    render (this.#sorting, this.#container);
+    render(this.#eventsList, this.#container);
+    render(this.#editForm, this.#eventsList.element);
 
-    for (let i = 0; i < this.points.length; i++) {
-      render(new WayPoint({points:this.points[i], destinationsData:this.destinationsData, offersData:this.offers}), this.EventsList.element);
+    for (let i = 0; i < this.#points.length; i++) {
+      render(new WayPoint({points:this.#points[i], destinationsData:this.#destinationsData, offersData:this.#offersData}), this.#eventsList.element);
+       // this.#renderPoint({points:this.#points[i], destinationsData:this.#destinationsData, offersData:this.#offersData});
     }
   }
+
+  /*#renderPoint (points, destinationsData, offersData) {
+    const pointComponent = new WayPoint({points, destinationsData, offersData});
+
+    render(pointComponent, this.#eventsList.element);
+  }*/
 }
