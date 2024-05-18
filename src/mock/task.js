e8cd8218f779b95ff5909c7f1466b1f787a52568
booblik2022 <booblik2022@gmail.com>
2024-05-18 13:05:12 +0300

    Увеличил количество точек

--- src/mock/task.js
+++ src/mock/task.js
@@ -46,6 +46,42 @@ const mockPoints = [
       '3'
     ],
     type: 'train'
+  },
+  {
+    id: '4',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T20:55:56.845Z',
+    dateTo: '2019-07-11T10:22:13.375Z',
+    destination: '1',
+    isFavorite: false,
+    offers: [
+      '1'
+    ],
+    type: 'taxi'
+  },
+  {
+    id: '5',
+    basePrice: 1200,
+    dateFrom: '2019-07-11T21:55:56.845Z',
+    dateTo: '2019-07-11T11:22:13.375Z',
+    destination: '2',
+    isFavorite: true,
+    offers: [
+      '2'
+    ],
+    type: 'flight'
+  },
+  {
+    id: '6',
+    basePrice: 1300,
+    dateFrom: '2019-07-12T22:55:56.845Z',
+    dateTo: '2019-07-11T12:22:13.375Z',
+    destination: '3',
+    isFavorite: false,
+    offers: [
+      '3'
+    ],
+    type: 'train'
   }
 ];
 
