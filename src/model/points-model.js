c361bbb202f4af769df0385eb7caf91b0bc67db3
booblik2022 <booblik2022@gmail.com>
2024-06-09 22:54:57 +0300

    подготовка модели используя паттерн Наблюдатель

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,13 +1,15 @@
 import { mockPoints, mockOffers, mockDestinations } from '../mock/mock-task.js';
-export default class PointsModel {
-  #points;
-  #offers;
-  #destinations;
+import Observable from '../framework/observable.js';
+
+export default class PointsModel extends Observable {
+  #points = [];
+  #offers = [];
+  #destinations = [];
   constructor() {
+    super();
     this.#points = [];
     this.#offers = [];
     this.#destinations = [];
-
   }
 
   init() {
@@ -27,6 +29,60 @@ export default class PointsModel {
   get destinations() {
     return this.#destinations;
   }
+
+  updatePoint(updateType, update) {
+    const index = this.#points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting point');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      update,
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  addPoint(updateType, update) {
+    this.#points = [update, ...this.#points];
+
+    this._notify(updateType, update);
+  }
+
+  deletePoint(updateType, update) {
+    const index = this.#points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting point');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType);
+  }
+
+  #adaptToClient = (point) => {
+    const adaptedPoint = {
+      ...point,
+      basePrice: point['base_price'],
+      dateFrom: point['date_from'],
+      dateTo: point['date_to'],
+      isFavorite: point['is_favorite'],
+    };
+
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
+  };
 
   getOffersByType(type) {
     return this.#offers.find((offer) => offer.type === type).offers;
