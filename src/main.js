4a4bb34f0d95e4c434fc70b3c3996b5b88445d89
booblik2022 <booblik2022@gmail.com>
2024-06-12 19:47:18 +0300

    Добавил новые презентеры

--- src/main.js
+++ src/main.js
@@ -1,18 +1,42 @@
-import {render} from './framework/render.js';
-import FiltersView from './view/filters-view.js';
 import MainPresenter from './presenter/main-presenter.js';
 import PointsModel from './model/points-model.js';
-import {generateFilter} from './mock/mock-filter.js';
+import FilterModel from './model/filter-model.js';
+import FilterPresenter from './presenter/filter-presenter.js';
+import NewPointButtonView from './view/new-point-button-view.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteSortingElement = document.querySelector('.trip-events');
-
+const newPointButton = document.querySelector('.trip-main__event-add-btn');
 const pointsModel = new PointsModel();
+const filterModel = new FilterModel();
+
+const mainPresenter = new MainPresenter({
+  container: siteSortingElement,
+  pointsModel,
+  filterModel,
+  onNewPointDestroy: handleNewPointFormClose,
+});
+
+const filterPresenter = new FilterPresenter({
+  filterContainer: siteFilterElement,
+  filterModel,
+  pointsModel,
+});
+
+const newPointButtonComponent = new NewPointButtonView({
+  button: newPointButton,
+  onClick: handleNewPointButtonClick,
+});
+
+function handleNewPointFormClose() {
+  newPointButtonComponent.template.disabled = false;
+}
+
+function handleNewPointButtonClick() {
+  mainPresenter.createPoint();
+  newPointButtonComponent.template.disabled = true;
+}
+
+filterPresenter.init();
+mainPresenter.init();
 pointsModel.init();
-
-const filters = generateFilter(pointsModel.points);
-
-const presenter = new MainPresenter({container: siteSortingElement, pointsModel: pointsModel});
-render(new FiltersView({filters}), siteFilterElement);
-
-presenter.init();
