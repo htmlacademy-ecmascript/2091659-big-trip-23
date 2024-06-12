abf7049d29ebb68f937fd077e8bfac2549c950db
booblik2022 <booblik2022@gmail.com>
2024-06-12 19:43:42 +0300

    Рефакторинг  отображения при отсутсвие точек

--- src/view/list-empty-message-view.js
+++ src/view/list-empty-message-view.js
@@ -1,13 +1,25 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { emptyMessageTextType } from '../const';
 
-function createListEmptyMessageTemplate() {
-  return (`
-  <p class="trip-events__msg">Click New Event to create your first point</p>
-  `);
-}
+const createListEmptyMessageTemplate = (filterType) => {
+  const currentPageMessageTextTipe = emptyMessageTextType[filterType];
+
+  return (
+    `<p class="trip-events__msg">
+      ${currentPageMessageTextTipe}
+    </p>`
+  );
+};
 
 export default class ListEmptyMessageView extends AbstractView {
+  #filterType = null;
+
+  constructor({filterType}) {
+    super();
+    this.#filterType = filterType;
+  }
+
   get template() {
-    return createListEmptyMessageTemplate();
+    return createListEmptyMessageTemplate(this.#filterType);
   }
 }
